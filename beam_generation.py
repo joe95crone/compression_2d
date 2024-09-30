9ec1c171e2dd23753d4527dac5b6d1bce7bd104b
Crone <Joe.Crone@stfc.ac.uk>
2024-09-30 14:58:43 +0100

    Added HDF5 beam file runs

--- beam_generation.py
+++ beam_generation.py
@@ -1,6 +1,18 @@
 # beam generation
 import numpy as np
 from scipy import constants
+import h5py
+import os
+
+class HDF5_read:
+	def __init__(self):
+		# Store an empty list for dataset names
+		self.names = []
+
+	def __call__(self, name, h5obj):
+		# only h5py datasets have dtype attribute, so we can search on this
+		if hasattr(h5obj,'dtype') and not name in self.names:
+			self.names += [name]
 
 class Beam_Generation:
     # want to pass either a filename or an image or a set of parameters
@@ -14,7 +26,14 @@ class Beam_Generation:
             self.sig_t = args[3]*constants.pico # s rms bunch duration 
             self.sig_e = args[4] # rms relative energy spread 
             self.E_mean = args[5]*constants.elementary_charge # SI mean energy
-        
+        elif init_type == "file":
+            self.LPS_data_tag = ['q','z','pz']
+            self.filename = args[0]
+            self.path = os.getcwd() + '\\Distributions\\'
+            self.file = h5py.File(self.path + self.filename,'r')
+            self.HDF5_in = HDF5_read()
+            self.file.visititems(self.HDF5_in)
+
     # Gaussian sampling with a cut off
     def gaussian_generator(self, mean, std, trunc):
         samples = []
@@ -29,6 +48,13 @@ class Beam_Generation:
                 samples.append(sample)
         return samples
     
+    # generate a cubic bunch in 1D using a series of dz values
+    def cubic_sige_generator(self, dz):
+        scale = (self.sig_e/(self.sig_t*constants.c))
+        fac = 0.85/(self.sig_e/1e-4)
+        return -scale*dz + (1/(fac*scale)**3)*dz**3
+
+    # generate a Gaussian bunch
     def gaussian_bunch(self):
         beam_sigz = np.array(self.gaussian_generator(0, self.sig_t*constants.c, self.cutoff))
         beam_sige = np.array(self.gaussian_generator(0, self.sig_e, self.cutoff))
@@ -36,7 +62,44 @@ class Beam_Generation:
         beam = [beam_sigz, beam_sige, beam_E]
         return beam
 
+    # generate a more realistic bunch with a cubic term in the energy spread
+    def cubic_sige_bunch(self):
+        beam_sigz = np.array(self.gaussian_generator(0, self.sig_t*constants.c, self.cutoff))
+        beam_sige = np.array(self.cubic_sige_generator(beam_sigz))
+        beam_E = np.full(self.npart, self.E_mean)
+        beam = [beam_sigz, beam_sige, beam_E]
+        return beam
+    
+    # get the bunch data in the standard three column format
+    # also sets spome of the parameters of the class
+    #! Unit conversion is based on Can's standard OPAL input 
+    def get_file_LPS_data(self):
+        dat_names = [] 
+        for tag in range(len(self.LPS_data_tag)):
+            for name in range(len(self.HDF5_in.names)):
+                if self.LPS_data_tag[tag] == self.HDF5_in.names[name].split('/')[1]:
+                    dat_names.append(self.HDF5_in.names[name])
+        HDF5_LPS = np.array([np.array(self.file[dat_names[i]]) for i in range(len(dat_names))])
+        # no macroparticles
+        self.npart = len(HDF5_LPS[0])
+        # charge (pC)
+        self.bunch_charge = -sum(HDF5_LPS[0])/constants.pico 
+        # SI Emean
+        self.Emean = np.mean(HDF5_LPS[2])*constants.value('electron mass energy equivalent in MeV')*constants.elementary_charge*constants.mega
+        delta_e = np.array([(HDF5_LPS[2][i]-np.mean(HDF5_LPS[2]))/np.mean(HDF5_LPS[2]) for i in range(len(HDF5_LPS[2]))])
+        Emean_beam = np.full(len(HDF5_LPS[2]), self.Emean)
+        beam = [np.array(HDF5_LPS[1]), delta_e, Emean_beam]
+        return beam
+
 #if __name__ == "__main__":
-#    beam_gen = Beam_Generation("param", 250, 1e4, 3, 1, 1e-4, 100)
-#    print(beam_gen.bunch_charge)
-#    Gbeam = beam_gen.gaussian_bunch()
+    # example generation run
+    #beam_gen_param = Beam_Generation("param", 250, 1e4, 3, 1, 1e-4, 100)
+    #print(beam_gen_param.bunch_charge)
+    #Gbeam = beam_gen_param.gaussian_bunch()
+    #print(Gbeam)
+
+    # example from file run
+    #beam_gen = Beam_Generation("file","#30.h5")
+    #beam = beam_gen.get_file_LPS_data()
+    #print(beam_gen.bunch_charge)
+    #print(beam)
--- compression_model.py
+++ compression_model.py
@@ -15,13 +15,27 @@ from scipy.optimize import minimize
 # Beamline Definition
 #---------------------
 
-SHINE_init = beam_generation.Beam_Generation("param", 100, 1e4, 6, 0.8638, 8.6e-4, 100.6e6)
-SHINE_beam = SHINE_init.gaussian_bunch()
+#SHINE_init = beam_generation.Beam_Generation("param", 100, 1e4, 6, 0.8638, 8.6e-4, 100.6e6)
+#SHINE_beam = SHINE_init.gaussian_bunch()
 
-SHINE_dict = munch.munchify({'L1': {'type': 'RF', 'f': 1300e6, 'V': 231e6, 'phi': np.radians(12.1)}, 'HC3': {'type': 'RF', 'f': 3900e6, 'V': 68e6, 'phi': np.radians(153)}, 'BC1': {'type': 'BC', 'R56': -62e-3} ,'L2': {'type': 'RF', 'f': 1300e6, 'V': 2168e6, 'phi': np.radians(25)}, 'BC2': {'type': 'BC', 'R56': -43.95e-3}, 'L3': {'type': 'RF', 'f': 1300e6, 'V': 2891e6, 'phi': 0}, 'L4': {'type': 'RF', 'f': 1300e6, 'V': 3614e6, 'phi': 0}})
+#SHINE_dict = munch.munchify({'L1': {'type': 'RF', 'f': 1300e6, 'V': 231e6, 'phi': np.radians(12.1)}, 'HC3': {'type': 'RF', 'f': 3900e6, 'V': 68e6, 'phi': np.radians(153)}, 'BC1': {'type': 'BC', 'R56': -62e-3} ,'L2': {'type': 'RF', 'f': 1300e6, 'V': 2168e6, 'phi': np.radians(25)}, 'BC2': {'type': 'BC', 'R56': -43.95e-3}, 'L3': {'type': 'RF', 'f': 1300e6, 'V': 2891e6, 'phi': 0}, 'L4': {'type': 'RF', 'f': 1300e6, 'V': 3614e6, 'phi': 0}})
 
-SHINE_tracker = element_tracker.Element_Track(SHINE_beam, 100, SHINE_dict)
-SHINE_tracker.synchrotron_radiation(20) 
+#SHINE_tracker = element_tracker.Element_Track(SHINE_beam, 100, SHINE_dict)
+#SHINE_tracker.synchrotron_radiation(20) 
+
+# parameter based generation
+#UKXFEL_init = beam_generation.Beam_Generation("param", 150, 1e4, 6, 6, 2.5e-4, 80e6)
+#UKXFEL_beam = UKXFEL_init.cubic_sige_bunch()
+
+# from file
+UKXFEL_init = beam_generation.Beam_Generation("file","#21.h5")
+UKXFEL_beam = UKXFEL_init.get_file_LPS_data()
+
+UKXFEL_dict = munch.munchify({'CAV1': {'type': 'RF', 'f': 650e6, 'V': 100e6, 'phi': np.radians(17)}, 'BC1': {'type': 'BC', 'R56': -0.45}})
+#UKXFEL_dict = munch.munchify({'BC1': {'type': 'BC', 'R56': 6}, 'HC3': {'type': 'RF', 'f': 3900e6, 'V': 20e6, 'phi': np.radians(40)}, 'CAV1': {'type': 'RF', 'f': 1300e6, 'V': 600e6, 'phi': np.radians(17)}, 'BC2': {'type': 'BC', 'R56': -0.09}, 'CAV2': {'type': 'RF', 'f': 1300e6, 'V': 2500e6, 'phi': np.radians(-5)}, 'BC3': {'type': 'BC', 'R56': 0.01}, 'CAV3': {'type': 'RF', 'f': 1300e6, 'V': 5000e6, 'phi': 0}})
+
+UKXFEL_tracker = element_tracker.Element_Track(UKXFEL_beam, UKXFEL_init.bunch_charge, UKXFEL_dict)
+
 #--------------
 # Standard Run
 #--------------
@@ -33,38 +47,53 @@ SHINE_tracker.synchrotron_radiation(20)
 #sig_t = SHINE_plt.beam_sigt_plt()
 #sig_e = SHINE_plt.beam_sige_plt()
 #Ipk = SHINE_plt.current_plt()
-#print(Ipk)
+#SHINE_plt.beam_sigt_sige_phase_space()
+
+UKXFEL_track = UKXFEL_tracker.track_1D()
+
+UKXFEL_plt = comp_plt.Track_Plot(UKXFEL_dict, UKXFEL_beam, UKXFEL_track, "UKXFEL", UKXFEL_init.bunch_charge, UKXFEL_init.npart)
+#E_mean = UKXFEL_plt.beam_energy_plt()
+sig_t = UKXFEL_plt.beam_sigt_plt()
+sig_e = UKXFEL_plt.beam_sige_plt()
+#Ipk = UKXFEL_plt.current_plt()
+UKXFEL_plt.beam_sigt_sige_phase_space(0)
+UKXFEL_plt.beam_sigt_sige_phase_space(1)
+UKXFEL_plt.beam_sigt_sige_phase_space()
+print(sig_t)
+
 #--------------
 # Optimisation
 #--------------
 
 # the optimization can be limited to certain variables by specifying the bounds as identical to the values 
 #SHINE_bounds = [(1300e6, 1300e6), (231e6, 231e6), (-np.pi, np.pi), (3900e6, 3900e6), (68e6, 68e6), (-np.pi, np.pi), (-10e-2, 10e-2), (1300e6, 1300e6), (2168e6, 2168e6), (np.radians(25), np.radians(25)), (-43.95e-3, -43.95e-3), (1300e6, 1300e6), (2891e6, 2891e6), (0, 0), (1300e6, 1300e6), (3614e6, 3614e6), (0, 0)]
-#SHINE_bounds = [(1300e6, 1300e6), (231e6, 231e6), (-np.pi, np.pi), (3900e6, 3900e6), (68e6, 68e6), (-np.pi, np.pi), (-10e-2, 10e-2), (1300e6, 1300e6), (2168e6, 2168e6), (-np.pi, np.pi), (-10e-2, 10e-2), (1300e6, 1300e6), (2891e6, 2891e6), (-np.pi, np.pi), (1300e6, 1300e6), (3614e6, 3614e6), (-np.pi, np.pi)]
 
+#UKXFEL_bounds = [(-10,10), (3900e6, 3900e6), (20e6, 20e6), (-np.pi, np.pi), (1300e6, 1300e6), (600e6, 600e6), (-np.pi, np.pi), (-1, 1), (1300e6, 1300e6), (2500e6, 2500e6), (-np.pi, np.pi), (-1, 1), (1300e6, 1300e6), (5000e6, 5000e6), (-np.pi, np.pi)]
+#UKXFEL_bounds = [(-10,10)]
 # penalty function values can be passed as a dictionary
 #pvals = {'p_sigt': 20e-15, 'p_sige': 1e-5}
+#pvals = {'p_sigt': 2000e-15, 'p_sige': 1e-5}
 
 # selected objective function
 #obj_func = 'sig_t'
 #obj_func = 'Ipk'
 
 # if no penalty function values are passed in the arguments then minimises the objective function
-#result = minimize(SHINE_tracker.opt_func, SHINE_tracker.initial_guess(), args = (obj_func), method = 'Nelder-Mead', bounds = SHINE_bounds, options = {'maxiter':2e4, 'fatol':0.01})
+#result = minimize(UKXFEL_tracker.opt_func, UKXFEL_tracker.initial_guess(), args = (obj_func, pvals), method = 'Nelder-Mead', bounds = UKXFEL_bounds, options = {'maxiter':2e4, 'fatol':0.01})
 
 #print('----------------')
 #print('Optimisation Results:')
 #print("penalty function/result: ", result.fun)
-#SHINE_init_param = np.array([list(SHINE_bounds[i]) for i in range(len(SHINE_bounds))])[:,0]
+#UKXFEL_init_param = np.array([list(UKXFEL_bounds[i]) for i in range(len(UKXFEL_bounds))])[:,0]
 
 #param_names = []
-#for k,v in SHINE_dict.items():
+#for k,v in UKXFEL_dict.items():
 #    param_names.append([k + '_' + s for s in list(v.keys())])
 #param_names = np.array([i for i in chain.from_iterable(param_names) if 'type' not in i])
 
-#SHINE_opt_param = np.array(result.x)
-#index_opt_param = np.flatnonzero((~np.equal(SHINE_init_param, SHINE_opt_param)).astype(int))
-#val_opt_param = SHINE_opt_param[index_opt_param]
+#UKXFEL_opt_param = np.array(result.x)
+#index_opt_param = np.flatnonzero((~np.equal(UKXFEL_init_param, UKXFEL_opt_param)).astype(int))
+#val_opt_param = UKXFEL_opt_param[index_opt_param]
 #name_opt_param = param_names[index_opt_param] 
 #print("Optimised Variables")
 #for i in range(len(name_opt_param)):
@@ -74,22 +103,23 @@ SHINE_tracker.synchrotron_radiation(20)
 #print('----------------')
 
 # run the solution
-#SHINE_track_opt = SHINE_tracker.track_1D()
+#UKXFEL_track_opt = UKXFEL_tracker.track_1D()
 
 #-------
 # PLOTS
 #-------
-#SHINE_plt = comp_plt.Track_Plot(SHINE_dict, SHINE_beam, SHINE_track_opt, "SHINE", SHINE_init.bunch_charge, SHINE_init.npart, save_plots=True)
+#UKXFEL_plt = comp_plt.Track_Plot(UKXFEL_dict, UKXFEL_beam, UKXFEL_track_opt, "UKXFEL", UKXFEL_init.bunch_charge, UKXFEL_init.npart, save_plots=True)
 
-#SHINE_plt.beam_sigt_sige_phase_space()
-#Ipk_final = SHINE_plt.current_plt()
+#UKXFEL_plt.beam_sigt_sige_phase_space(0)
+#UKXFEL_plt.beam_sigt_sige_phase_space()
+#Ipk_final = UKXFEL_plt.current_plt()
 
-#E_mean = SHINE_plt.beam_energy_plt()
-#sig_t = SHINE_plt.beam_sigt_plt()
-#sig_e = SHINE_plt.beam_sige_plt()
-#Ipk = SHINE_plt.beam_Ipk_plt()
+#E_mean = UKXFEL_plt.beam_energy_plt()
+#sig_t = UKXFEL_plt.beam_sigt_plt()
+#sig_e = UKXFEL_plt.beam_sige_plt()
+#Ipk = UKXFEL_plt.beam_Ipk_plt()
 #print(sig_t)
 #print(Ipk)
-#plt.show()
+plt.show()
 
 
